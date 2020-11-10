f4af2cae5ee242f583fab6f7d968b0871f75080d
Your Username <you@example.com>
2020-11-10 11:00:09 -0500

    commit tree 9ace15511e783dfc840e2a633fc115f2ba74d854

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,54 @@
+require 'pry'
 
+class CashRegister 
+
+    attr_accessor :total, :discount, :items, :last_transaction 
+    
+
+    
+    
+    def initialize(discount = 0) 
+        @total = 0
+        @discount = discount 
+        @items = []
+    end 
+
+    def add_item(title, price, quantity = 1) 
+        @last_transaction = price * quantity
+        @total = @total + @last_transaction
+
+        
+         quantity.times do 
+            @items << title 
+         end  
+        
+    end 
+
+    def apply_discount 
+
+        if @discount == 0 
+            return "There is no discount to apply." 
+        else 
+        
+        amount_off = (@total * (@discount.to_f/100)).to_i  
+        @total = @total - amount_off
+    
+        return "After the discount, the total comes to $#{@total}."  
+        end 
+        
+    end 
+
+    def items
+       @items 
+     end 
+
+     def void_last_transaction 
+        @total = @total - @last_transaction 
+        
+
+
+        
+     end 
+
+
+end 
